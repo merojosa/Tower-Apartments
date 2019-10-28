3c98f795b8f66b66c7d950a4886e663aa0534789
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2019-10-28 13:44:53 -0600

    Adaptado los tests.

--- src/test/java/security_test/AccessTest.java
+++ src/test/java/security_test/AccessTest.java
@@ -1,73 +1,41 @@
 package security_test;
 
-import java.util.HashSet;
-
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
-import security.AccessProxy;
 import security.InternetImplementation;
 import security.InternetService;
-import security.Proxy;
-import security.WebsitesProxy;
 
 public class AccessTest 
 {
-	
-	private InternetService service;
-	private Proxy accessProxy;
-	private Proxy websitesProxy;
-
-	
-	@Before
-	public void initObjects()
+	@Test
+	public void testValidUrl()
 	{
-		service = new InternetImplementation(); 
-		accessProxy = new AccessProxy(service);
-		
-		websitesProxy = new WebsitesProxy(new AccessProxy(service));
-		HashSet<String> forbiddenWebsites = new HashSet<String>();
-		forbiddenWebsites.add("xxx.com");
-		((WebsitesProxy) websitesProxy).setForbiddenWebsites(forbiddenWebsites);
+		InternetService service = new InternetImplementation();
+		Assert.assertTrue(service.accessWebsite("facebook.com"));
 	}
 	
-	
 	@Test
-	public void allowAccessProxyTest() 
+	public void testBlockedUrl()
 	{
-		Assert.assertTrue(accessProxy.accessWebsite("facebook.com"));
+		InternetService service = new InternetImplementation();
+		Assert.assertFalse(service.accessWebsite("xxx.com"));
 	}
 	
-
 	@Test
-	public void allowWebsitesProxyTest() 
-	{		
-		Assert.assertTrue(websitesProxy.accessWebsite("facebook.com"));
-	}
-	
-	
-	@Test
-	public void denyAccessProxyTest()
+	public void testCountAccesses()
 	{
-		Assert.assertTrue(accessProxy.accessWebsite("facebook.com"));
-		Assert.assertTrue(accessProxy.accessWebsite("hotmail.com"));
-		Assert.assertTrue(accessProxy.accessWebsite("twitter.com"));
-		Assert.assertTrue(accessProxy.accessWebsite("facebook.com"));
-		Assert.assertTrue(accessProxy.accessWebsite("facebook.com"));
-		Assert.assertTrue(accessProxy.accessWebsite("gmail.com"));
-		Assert.assertTrue(accessProxy.accessWebsite("google.com"));
-		Assert.assertTrue(accessProxy.accessWebsite("instagram.com"));
-		Assert.assertTrue(accessProxy.accessWebsite("teletica.com"));
-		Assert.assertTrue(accessProxy.accessWebsite("repretel.com"));
-		// Denied.
-		Assert.assertFalse(accessProxy.accessWebsite("facebook.com"));
-	}
-	
-	
-	@Test
-	public void denyWebsitesProxyTest()
-	{
-		Assert.assertFalse(websitesProxy.accessWebsite("xxx.com"));
+		InternetService service = new InternetImplementation();
+		service.accessWebsite("facebook.com");
+		service.accessWebsite("hotmail.com");
+		service.accessWebsite("twitter.com");
+		service.accessWebsite("hotmail.com");
+		service.accessWebsite("gmail.com");
+		service.accessWebsite("facebook.com");
+		service.accessWebsite("instagram.com");
+		service.accessWebsite("google.com");
+		service.accessWebsite("drive.com");
+		service.accessWebsite("github.com");
+		Assert.assertFalse(service.accessWebsite("facebook.com"));
 	}
 }
