a5d9ea37f21d6d0da5e690d966e744f9926d43ee
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2019-11-06 00:37:12 -0600

    Construcción del apartamento
    Se agregó en el builder un add para los comportamientos.  Para ello se ocupa de un mediator como atributo.
    
    Dentro de la lógica para agregar comportamientos, se definió que necesita exisitr los dispositivos involucrados; caso contrario no se agrega.

--- src/main/actuators_sensors/java/management/Apartment.java
+++ src/main/actuators_sensors/java/management/Apartment.java
@@ -3,15 +3,18 @@ package management;
 import java.util.ArrayList;
 import java.util.List;
 
+import actions.ActionDeviceStrategy;
 import construction_devices.Device;
 
 public class Apartment 
 {
 	private List<Device> devices;
+	private Mediator mediator;
 	
 	private Apartment()
 	{
 		devices = new ArrayList<Device>();
+		mediator = new Mediator();
 	}
 	
 	
@@ -34,6 +37,21 @@ public class Apartment
 		public Builder addDevice(Device device)
 		{
 			apartment.devices.add(device);
+			return this;
+		}
+		
+		public Builder addBehavior(ActionDeviceStrategy action, ActionDeviceStrategy answer)
+		{
+			// To add a behavior, both devices involved need to exist in the apartment.
+			if( apartment.devices.contains(action.getDevice()) && apartment.devices.contains(answer.getDevice()) )
+			{
+				apartment.mediator.addBehavior(action, answer);
+			}
+			else
+			{
+				// Testing
+				System.out.println("No se puede configurar el comportamiento porque faltan dispositivos.");
+			}
 			return this;
 		}
 		
