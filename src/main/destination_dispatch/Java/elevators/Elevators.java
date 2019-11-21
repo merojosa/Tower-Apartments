81da3107fc22ea583f9f0088c80e4dd910698848
Dasc1227 <davidsolco@gmail.com>
2019-11-20 21:03:52 -0600

    Builder de Elevator

--- src/main/destination_dispatch/Java/elevators/Elevators.java
+++ src/main/destination_dispatch/Java/elevators/Elevators.java
@@ -2,15 +2,42 @@ package elevators;
 
 public class Elevator{
 	
-	int currentFloor; 
-	int destinationFloor; 
+	public int currentFloor; 
+	public int destinationFloor; 
 	public userType type; 
-	int id; 
+	public int id; 
 	
-	//
-	public static class Builder{
+	public Elevator() {
 		
 	}
+	
+	
+	public static class Builder{
+		private Elevator elevator;
+		
+		public Builder(int id) {
+			this.elevator = new Elevator();
+			this.elevator.id = id;
+			return this;
+		}
+		
+		public Builder setConfiguration(int currentfloor1, int dest) {
+			this.elevator.currentFloor = currentfloor1;
+			this.elevator.destinationFloor = dest;
+			return this;
+		}
+		
+		public Builder setType(userType type2) {
+			this.elevator.type = type2;
+			return this;
+		}
+		
+		public Elevator build() {
+			return this.elevator;
+		}
+	}
+	
+
 
 }
 
